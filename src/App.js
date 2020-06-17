df141137373c75a132aa4c5bf676a8570c0d8f1e
SnehaAgg0212 <aggarwalsneha1999@gmail.com>
2020-06-17 12:42:58 +0530

    signUp page

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import logo from './logo.png';
+import  './App.css';
+import Form from './Form'
+
+class App extends Component {
+ 
+    render(){
+  return (
+          <div className="App">
+             <header className = "App-header">
+                 <img src={logo} className= "image" alt="logo"/>
+          
+          Enter the details here
+          
+               <Form />
+          <br/>
+          <br/>
+          
+              
+                
+              <a
+                className="App-link"
+                href="http://examination.igdtuw.ac.in/Examination_v2/users/login"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Proceed towards logIn
+              </a>
+              </header>
+          </div>
+  );
+}
+}
+export default App;
